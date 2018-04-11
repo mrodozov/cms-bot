c6b760c82cec6df34d622410588d24d6c853fcc4
mrodozov <mrodozov@cern.ch>
2018-04-11 12:20:56 +0200

    Push with commented old implementations

--- getWorkflowStatsFromES.py
+++ getWorkflowStatsFromES.py
@@ -1,7 +1,9 @@
-import json, sys
+#!/usr/bin/env python
+import json, sys, urllib3
 from time import time
-from es_utils import get_payload
-from ROOT import *
+from datetime import datetime
+from es_utils import get_payload_kerberos
+#from ROOT import *
 
 '''
 this program uses pyROOT, no brainer would be to set cmsenv before running it
@@ -12,82 +14,83 @@ def _format(s, **kwds):
 
 def getWorkflowStatsFromES(release='*', arch='*', lastNdays=7, page_size=0):
 
-    query_url = 'http://cmses-master02.cern.ch:9200/relvals_stats_*/_search'
+    query_url = 'https://es-cmssdt.cern.ch/krb/cmssdt-relvals_stats_summary*/_search?scroll=1m'
 
-    query_datsets = """
-        {
-          "query": {
-            "filtered": {
-              "query": {
-                "bool": {
-                  "should": [
-                    {
-                      "query_string": {
-                        "query": "release:%(release_cycle)s AND architecture:%(architecture)s", 
-                        "lowercase_expanded_terms": false
-                      }
-                    }
-                  ]
-                }
-              },
-              "filter": {
-                "bool": {
-                  "must": [
-                    {
-                      "range": {
-                        "@timestamp": {
-                          "from": %(start_time)s,
-                          "to": %(end_time)s
-                        }
-                      }
-                    }
-                  ]
+    query_datsets = """{
+    "query": {
+        "bool": {
+            "filter": [{ "range": { "@timestamp": { "gte": "%(start_time)s", "lt": "%(end_time)s"}}}],
+            "must": { "query_string": {"query": "release:/%(release_cycle)s.*/ AND architecture:/%(architecture)s.*/ "}
+        }
+    }
+    },
+    "from" : %(from)s,
+    "size" : %(page_size)s
+    }"""
+
+    queryInfo = {}
+    millisecday=86400*1000
+    queryInfo["end_time"] = int(time()*1000)-(millisecday*7)
+    queryInfo["start_time"] = queryInfo["end_time"] - int(millisecday * lastNdays)
+    #queryInfo["start_time"] = 1521720803
+
+    #print queryInfo["start_time"], queryInfo["end_time"]
+
+    queryInfo["architecture"] = arch.lower()
+    queryInfo["release_cycle"] = release.lower()
+    queryInfo["from"] = 0
+    queryInfo["page_size"] = 10000 #default
+    #for i in queryInfo:
+    #print _format(query_datsets, **queryInfo)
+
+    query= """{
+      "query": {
+        "bool": {
+          "filter": [
+            {
+              "range": {
+                "@timestamp": {
+                  "gte": "%(start_time)s",
+                  "lt": "%(end_time)s"
                 }
               }
             }
-          },
-          "from": %(from)s,
-          "size": %(page_size)s
+          ],
+          "must": {
+            "query_string": {
+              "query": "release:/%(release_cycle)s.*/ AND architecture:/%(architecture)s.*/ "
+            }
+          }
         }
-        """
-    datasets = {}
-    ent_from = 0
-    json_out = []
-    info_request = False
-    queryInfo = {}
+      },
+      "from": 0,
+      "size": 10000
+      }
+    """
+    #print query
+    query = _format(query, **queryInfo)
+    #print query
+    es_data = get_payload_kerberos(query_url, query)
+    #print es_data
 
-    queryInfo["end_time"] = int(time() * 1000)
-    queryInfo["start_time"] = queryInfo["end_time"] - int(86400 * 1000 * lastNdays)
-    queryInfo["architecture"] = arch
-    queryInfo["release_cycle"] = release
-    queryInfo["from"] = 0
+    '''
+    scroll_size = es_data['hits']['total']
+    final_scroll_data = []
+    final_scroll_data.append(es_data['hits']['hits'])
 
-    if page_size < 1:
-        info_request = True
-        queryInfo["page_size"] = 2
-    else:
-        queryInfo["page_size"] = page_size
+    while (scroll_size > 0):
+        scroll_id = es_data['_scroll_id']
+        scroll_query = {"scroll_id": str(scroll_id), "scroll": "1m"}
 
-    total_hits = 0
+        es_data = json.loads(get_payload_kerberos(scroll_url, json.dumps(scroll_query)))
+        scroll_size = len(es_data['hits']['hits'])
+        if (scroll_size > 0):
+            final_scroll_data.append(es_data['hits']['hits'])
 
-    while True:
-        queryInfo["from"] = ent_from
-        es_data = get_payload(query_url, _format(query_datsets, **queryInfo))  # here
-        content = json.loads(es_data)
-        content.pop("_shards", None)
-        total_hits = content['hits']['total']
-        if info_request:
-            info_request = False
-            queryInfo["page_size"] = total_hits
-            continue
-        hits = len(content['hits']['hits'])
-        if hits == 0: break
-        ent_from = ent_from + hits
-        json_out.append(content)
-        if ent_from >= total_hits:
-            break
-
-    return json_out[0]['hits']['hits']
+    print json.dumps(final_scroll_data, indent=2, sort_keys=True, separators=(',', ': '))
+    #exit(1)
+    '''
+    return es_data['hits']['hits']
 
 '''
 have a function that narrows the result to fields of interest, described in a list and in the given order
@@ -95,8 +98,6 @@ have a function that narrows the result to fields of interest, described in a li
 
 def filterElasticSearchResult(ES_result=None, list_of_fields=None):
 
-    #arch = ES_result[0]['_source']['architecture']
-    #print arch
     final_struct = {}
 
     for element in ES_result:
@@ -107,7 +108,6 @@ def filterElasticSearchResult(ES_result=None, list_of_fields=None):
         stamp = source_object['@timestamp']
         flow = source_object['workflow']
         step = source_object['step']
-        
 
         if not stamp in final_struct:
             final_struct.update({stamp: {}})
@@ -173,13 +173,15 @@ def compareMetrics(firstObject=None, secondObject=None,workflow=None,stepnum=Non
 
 if __name__ == "__main__":
 
+    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
     opts = None
     release = None
     fields = ['time', 'rss_max', 'cpu_avg', 'rss_75' , 'rss_25' , 'rss_avg' ]
 
     arch = 'slc6_amd64_gcc630'
     days = int(sys.argv[5])
-    page_size = 0
+    #page_size = 0
     limit = 20
 
     release_one = sys.argv[1]
@@ -190,19 +192,30 @@ if __name__ == "__main__":
     step_n = None
     if len(sys.argv) > 6: wf_n = sys.argv[6]
     if len(sys.argv) > 7: step_n = sys.argv[7]
-    print wf_n, step_n
+    #print wf_n, step_n
     
-    json_out_first = getWorkflowStatsFromES(release_one, archone, days, page_size)
-    json_out_second = getWorkflowStatsFromES(release_two, archtwo, days, page_size)
+    json_out_first = getWorkflowStatsFromES(release_one, archone, days)
+    with open('firstout.json', "w") as firstfileout:
+        firstfileout.write(json.dumps(json_out_first,indent=2, sort_keys=True, separators=(',', ': ')))
 
     filtered_first = filterElasticSearchResult(json_out_first, fields)
+    with open('first.json', "w") as firstfile:
+        firstfile.write(json.dumps(filtered_first,indent=2, sort_keys=True, separators=(',', ': ')))
+
+    json_out_second = getWorkflowStatsFromES(release_two, archtwo, days)
+    with open('secondout.json', "w") as secondfileout:
+        secondfileout.write(json.dumps(json_out_second,indent=2, sort_keys=True, separators=(',', ': ')))
+
     filtered_second = filterElasticSearchResult(json_out_second, fields)
+    with open('second.json', "w") as secondfile:
+        secondfile.write(json.dumps(filtered_second,indent=2, sort_keys=True, separators=(',', ': ')))
 
     comp_results = compareMetrics(filtered_first, filtered_second, wf_n, step_n)
+    #print comp_results
     print json.dumps(comp_results, indent=2, sort_keys=True, separators=(',', ': '))
-
-    for hist in comp_results:
-        histo = TH1F(hist, hist, 100000, -5000, 5000)
-        for i in comp_results[hist]:
-            histo.Fill(i)
-        histo.SaveAs(hist+".root")
+    
+    #for hist in comp_results:
+    #    histo = TH1F(hist, hist, 100000, -5000, 5000)
+    #    for i in comp_results[hist]:
+    #        histo.Fill(i)
+    #    histo.SaveAs(hist+".root")
