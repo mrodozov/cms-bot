12a11b0e3863012d695bee03062e1631549b16d5
Mircho Rodozov <mrodozov@cern.ch>
2018-11-05 17:07:19 +0100

    update again bootstrap

--- cvmfs_deployment/bootstrap_dir_for_arch.sh
+++ cvmfs_deployment/bootstrap_dir_for_arch.sh
@@ -3,6 +3,10 @@
 INSTALL_PATH=$1
 ARCHITECTURE=$2
 
+#  docker run doesnt exist inside the script.
+
+source ${INSTALL_PATH}/cms-bot/cvmfs_deployment/docker_proot_function.sh
+
 if [ $(ls -al ${INSTALL_PATH}/${ARCHITECTURE}/external/rpm/*/etc/profile.d/init.sh 2>/dev/null 1>/dev/null ; echo $? ; ) -ne 0 ] ; then
     echo 'boostrap not found, installing'
     wget --tries=5 --waitretry=60 -O ${INSTALL_PATH}/bootstrap.sh http://cmsrep.cern.ch/cmssw/repos/bootstrap.sh
