2a989dd1db424806426ff9321b7cb14917e3db88
Mircho Rodozov <mrodozov@cern.ch>
2018-11-05 16:43:35 +0100

    pass arguments to bootstrap script

--- cvmfs_deployment/bootstrap_dir_for_arch.sh
+++ cvmfs_deployment/bootstrap_dir_for_arch.sh
@@ -1,6 +1,10 @@
 #  bootstrap - check if it exists and install if doesn't exist
+
+INSTALL_PATH=$1
+ARCHITECTURE=$2
+
 if [ $(ls -al ${INSTALL_PATH}/${ARCHITECTURE}/external/rpm/*/etc/profile.d/init.sh 2>/dev/null 1>/dev/null ; echo $? ; ) -ne 0 ] ; then
     echo 'boostrap not found, installing'
-    wget --tries=5 --waitretry=60 -O ${WORKDIR}/bootstrap.sh http://cmsrep.cern.ch/cmssw/repos/bootstrap.sh
+    wget --tries=5 --waitretry=60 -O ${INSTALL_PATH}/bootstrap.sh http://cmsrep.cern.ch/cmssw/repos/bootstrap.sh
     dockerrun "sh -ex ${WORKDIR}/bootstrap.sh -a ${ARCHITECTURE} -repository ${RPMS_REPO} -path ${WORKDIR} setup"
 fi
