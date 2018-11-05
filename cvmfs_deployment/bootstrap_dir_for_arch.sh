c880b4fbbcce61356475b2a37521321cfd217076
Mircho Rodozov <mrodozov@cern.ch>
2018-11-05 17:02:20 +0100

    update again bootstrap

--- cvmfs_deployment/bootstrap_dir_for_arch.sh
+++ cvmfs_deployment/bootstrap_dir_for_arch.sh
@@ -6,5 +6,5 @@ ARCHITECTURE=$2
 if [ $(ls -al ${INSTALL_PATH}/${ARCHITECTURE}/external/rpm/*/etc/profile.d/init.sh 2>/dev/null 1>/dev/null ; echo $? ; ) -ne 0 ] ; then
     echo 'boostrap not found, installing'
     wget --tries=5 --waitretry=60 -O ${INSTALL_PATH}/bootstrap.sh http://cmsrep.cern.ch/cmssw/repos/bootstrap.sh
-    dockerrun "sh -ex ${WORKDIR}/bootstrap.sh -a ${ARCHITECTURE} -repository ${RPMS_REPO} -path ${WORKDIR} setup"
+    dockerrun "sh -ex ${INSTALL_PATH}/bootstrap.sh -a ${ARCHITECTURE} -repository ${RPMS_REPO} -path ${INSTALL_PATH} setup"
 fi
