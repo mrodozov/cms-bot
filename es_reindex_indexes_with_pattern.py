5d0d083532b8bf4a127016d6531fb99f33b00092
Mircho Rodozov <mrodozov@cern.ch>
2019-07-11 13:53:44 +0200

    Add delay of 10 seconds between steps

--- es_reindex_indexes_with_pattern.py
+++ es_reindex_indexes_with_pattern.py
@@ -3,6 +3,7 @@ from __future__ import print_function
 from es_utils import get_indexes, delete_index, send_request
 import sys
 import json
+from time import sleep
 
 if __name__ == "__main__":
 
@@ -17,7 +18,7 @@ if __name__ == "__main__":
         for j in list_of_recs:
             if 'cmssdt-' in j:
                 indexes_name_only.append(j)
-    
+
     print('indexes names only')
     print(indexes_name_only)
     for i in indexes_name_only:
@@ -28,16 +29,21 @@ if __name__ == "__main__":
         print('request for reindex body: ', request_data)
         request_finished_properly = send_request('_reindex/', request_data, method='POST')
         if request_finished_properly:
+            print('first reindexing complete, delete')
             delete_index(current_idx)
         else:
             print('reindexing failed for ', current_idx, ' to ',tmp_idx, ', crash the jenkins job')
             exit(-1)
+
+        # index was just created, wait 10 seconds before trying to read it. requests were failing otherwise
+        # assumption is not readable yet ()
+        sleep(10)
 
         request_data = {"source":{"index": tmp_idx }, "dest":{"index": current_idx} }
         request_finished_properly = send_request('_reindex/', request_data, method='POST')
         if request_finished_properly:
             delete_index(tmp_idx)
         else:
-            print('reindexing failed for ', tmp_idx, ' to ', current_idx, ', crash the jenkins job, try execute the request manually')
+            print('reindexing failed for ', tmp_idx, ' to ', current_idx, ', crash the jenkins job, try manually')
             exit(-1)
 
