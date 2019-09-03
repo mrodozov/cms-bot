8a596d80dafda2f273cdd50850b62821dd0ebb8f
Mircho Rodozov <mrodozov@cern.ch>
2019-09-03 16:54:06 +0200

    Remove prints

--- parse_extra_pr_params.py
+++ parse_extra_pr_params.py
@@ -11,64 +11,53 @@ regexp_map = { "workflows" : WF_PATTERN, "cmssw_pr" : CMSSW_PR_PATTERN, "cmsdist
 full_cmssw_pattern = "(true|false)"
 cmssw_extra_prs_ONLY_pttrn = "(cms-sw\/cmssw#+[0-9][0-9]*)"
 cmsdist_extra_prs_ONLY_pttrn = "(cms-sw\/cmsdist#+[0-9][0-9]*)"
+cmsdata_extra_prs = ("cmsdata\/[a-zA-Z0-9]#+[0-9][0-9]*")
 
 cherry_picked_map = { "workflows" : WF_PATTERN , "arch" : ARCH_PATTERN , "cmssw_queue_pattern" : CMSSW_QUEUE_PATTERN,
                       "cmsdist_prs" : cmsdist_extra_prs_ONLY_pttrn, # matches more then one
                       "cmssw_prs" : cmssw_extra_prs_ONLY_pttrn, # matches more then one
+                      "cmsdata_prs" : cmsdata_extra_prs,
                       "full_cmssw" : full_cmssw_pattern, #
                       "cmssw_release_queue_pattern" : CMSSW_RELEASE_QUEUE_PATTERN
                       }
 
 def parse_extra_params(full_comment):
-    # first line
+    # check first line
     if not "test parameters" in full_comment.splitlines()[0]:
         print("not a proper multiline comment")
-        #return "something mathing the default structure but empty, or maybe return None"
-        return None
+        return {}  # return empty
 
     matched_extra_args = dict()
-
     for l in full_comment.splitlines():
 
-        extra_vals = None
-        print('\n --- \n', l)
         for k in cherry_picked_map:
             pttrn = cherry_picked_map[k]
             if l.find('=') is -1:
                 # only the first line should match this, or the = is missing in the comment, skip if it's missing
                 continue
 
-            print('  keyword:', k, '\n  pattern: ', pttrn, '\n  full line: ' , l)
-            line_args = l.replace(" ","")
-            print(line_args, ' lenght: ', len(line_args))
+            line_args = l.replace(" ", "")
 
             if line_args.find(',') is -1:
                 extra_vals = [line_args.split("=")[1]]
             else:
                 line_args = line_args.replace("=", " ")
                 extra_vals = [x for x in re.compile('\s*[,|\s+]\s*').split(line_args)]
-            print('evals', extra_vals)
-
             list_of_matches = []
 
-            print( 'before', list_of_matches)
             for v in extra_vals:
                 if re.match(pttrn, v):
                     list_of_matches.append(v)
-                    print('matches', v)
-            print('after', list_of_matches)
 
             if list_of_matches:
                 matched_extra_args[k] = list_of_matches
 
-
-    print('struct is')
-    print(json.dumps(matched_extra_args, indent=1, sort_keys=True))
-
+    return matched_extra_args
 
 with open("multiline_github_comment_example.txt") as gh_comment_data:
     file_lines = gh_comment_data.read()
 
 #print(TEST_REGEXP)
 #print(CMSDIST_PR_PATTERN)
-parse_extra_params(file_lines)
+multiline_args = parse_extra_params(file_lines)
+print(json.dumps(multiline_args, indent=1, sort_keys=True))
